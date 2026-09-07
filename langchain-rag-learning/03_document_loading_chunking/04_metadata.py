799e16e8eee329662e2a5553c1e97de90ca5bf05
Khushi Gupta <kg7825881@gmail.com>
2026-09-07 09:02:41 +0530

    Update in metadata file

--- langchain-rag-learning/03_document_loading_chunking/04_metadata.py
+++ langchain-rag-learning/03_document_loading_chunking/04_metadata.py
@@ -1,22 +1,37 @@
+from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
 
-# Load
-loader = PyPDFLoader(
-    "langchain-rag-learning/03_document_loading_chunking/documents/Cardiac Arrest.pdf"
+MARKDOWN_PATH = Path("03_document_loading_chunking/outputs/Cardiac_Arrest.md")
+
+#Load the Markdown 
+markdown_text = MARKDOWN_PATH.read_text(
+    encoding="utf-8"
 )
 
-documents = loader.load()
+#Create Langchain Document
+document = Document(
+    page_content=markdown_text,
+    metadata={
+        "source": "Cardiac Arrest.pdf",
+        "extraction_type":  "pymupdf4llm",
+        "format": "markdown",
+        "domain":"healthcare",
+        "topic": "cardiac_arrest"
+    }
+)
 
-# Add custom metadata
+documents=[document]
 
-for document in documents:
+print(
+    f"Loaded {len(documents)} LangChain Document"
+)
 
-    document.metadata["domain"] = "healthcare"
-    document.metadata["document_type"] = "research_paper"
-    document.metadata["topic"] = "cardiac_arrest_prediction"
-    document.metadata["subtopic"] = "machine_learning"
-    document.metadata["module"] = "langchain_learning"
+print(
+    f"Total Characters {len(document.page_content)}"
+)
+
 
 # Chunk
 
