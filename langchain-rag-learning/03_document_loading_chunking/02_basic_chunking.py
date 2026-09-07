5e128965b7c347ef27f7294c7deb576039bcc1c6
Khushi Gupta <kg7825881@gmail.com>
2026-09-07 08:50:56 +0530

    Update in basic chunking file

--- langchain-rag-learning/03_document_loading_chunking/02_basic_chunking.py
+++ langchain-rag-learning/03_document_loading_chunking/02_basic_chunking.py
@@ -1,13 +1,38 @@
+from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
 
-# Load a PDF document 
-loader = PyPDFLoader(
-    "langchain-rag-learning\\03_document_loading_chunking\\documents\\Cardiac Arrest.pdf"
+MARKDOWN_PATH = Path("03_document_loading_chunking/outputs/Cardiac_Arrest.md")
+
+#Load the Markdown 
+markdown_text = MARKDOWN_PATH.read_text(
+    encoding="utf-8"
 )
 
-documents = loader.load()
+#Create Langchain Document
+document = Document(
+    page_content=markdown_text,
+    metadata={
+        "source": "Cardiac Arrest.pdf",
+        "extraction_type":  "pymupdf4llm",
+        "format": "markdown",
+        "domain":"healthcare",
+        "topic": "cardiac_arrest"
+    }
+)
 
+documents=[document]
+
+print(
+    f"Loaded {len(documents)} LangChain Document"
+)
+
+print(
+    f"Total Characters {len(document.page_content)}"
+)
+
+#Create the splitter
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
     chunk_overlap=0
@@ -20,9 +45,12 @@ chunks = text_splitter.split_documents(
 print("Total Chunks:", len(chunks))
 
 #Inspect chunks 
-for i, chunks in enumerate(chunks[:5]):
-    print("="*70)
+for i, chunk in enumerate(chunks[:5]):
+
+    print("\n" + "=" * 70)
     print("Chunk:", i)
-    print(chunks.page_content)
-    print("METADATA:")
-    print(chunks.metadata)
+
+    print(chunk.page_content)
+
+    print("\nMETADATA:")
+    print(chunk.metadata)
