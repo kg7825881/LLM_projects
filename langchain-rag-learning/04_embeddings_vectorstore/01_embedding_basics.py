db3db9210f65a061de23a585d58bbcace56f39f0
Khushi Gupta <kg7825881@gmail.com>
2026-09-07 11:04:03 +0530

    Adding embedding basics

--- langchain-rag-learning/04_embeddings_vectorstore/01_embedding_basics.py
+++ langchain-rag-learning/04_embeddings_vectorstore/01_embedding_basics.py
@@ -1,6 +1,6 @@
 texts = [
     "Random Forest achieved high accuracy.",
-    "Random Forest performed well for cardiac arrest prediction.",
+    "Random Forest performed well for cardiac arrest prediction",
     "Bananas are a type of fruit."
 ]
 
